18529e75acac1b5ba61f47513b9abe556a581775
bur <belay.bura@gmail.com>
2021-07-18 16:41:56 +0300

    ..

--- src/Store/Auth/auth.actions.js
+++ src/Store/Auth/auth.actions.js
@@ -23,35 +23,35 @@ var expires = "expires=" + d.toUTCString();
 
 export const loginUser = (userData, history, checked) => (dispatch) => {
     dispatch({type: Types.AUTH_LOADING});
-    dispatch({
-        type: Types.SET_AUTHENTICATED,
-        payload: {}
-    });
-    history.push("/");
+    // dispatch({
+    //     type: Types.SET_AUTHENTICATED,
+    //     payload: {}
+    // });
+    // history.push("/");
 
-    // axios
-    //     .post(gConsts.API_ROOT + "users/login", userData)
-    //     .then((res) => {
-    //         axios.defaults.headers.common['Authorization'] = res.data.token;
-    //         if (checked === true) {
-    //             saveTokenToLocalStorage(res.data.token, res.data.user);
-    //         }
-    //         dispatch({
-    //             type: Types.SET_AUTHENTICATED,
-    //             payload: res.data.user
-    //         });
-    //         if (res.data.user.role === "Admin") history.push("/admin")
-    //         else
-    //             // else if (response.data.user.role === "User")
-    //             history.push("/");
-    //     })
-    //     .catch((err) => {
-    //         // console.log(err.response, "errrrrr")
-    //         dispatch({
-    //             type: Types.SET_AUTH_ERROR,
-    //             // payload: err.response.Data
-    //         });
-    //     });
+    axios
+        .post(gConsts.API_ROOT + "users/login", userData)
+        .then((res) => {
+            axios.defaults.headers.common['Authorization'] = res.data.token;
+            if (checked === true) {
+                saveTokenToLocalStorage(res.data.token, res.data.user);
+            }
+            dispatch({
+                type: Types.SET_AUTHENTICATED,
+                payload: res.data.user
+            });
+            if (res.data.user.role === "Admin") history.push("/admin")
+            else
+                // else if (response.data.user.role === "User")
+                history.push("/");
+        })
+        .catch((err) => {
+            // console.log(err.response, "errrrrr")
+            dispatch({
+                type: Types.SET_AUTH_ERROR,
+                // payload: err.response.Data
+            });
+        });
 };
 
 export const signupUser = (newUserData, history) => (dispatch) => {
